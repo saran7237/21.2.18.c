331f33a1f162eba4dae3d14f927a7c26eb05cf57
saran7237 <36067371+saran7237@users.noreply.github.com>
2018-02-21 16:01:45 +0530

    Create 2p.21.2.c

---
+++ 2p.21.2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b,c;
+printf("enter the ood number");
+scanf("%d",&a);
+printf("enter the even number");
+sacnf("%d",&b);
+c=a+b;
+printf("enter the total numbers");
+scanf("%d",&c);
+getch ();
+return 0;
+}
