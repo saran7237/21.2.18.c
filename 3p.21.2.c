3941b1d4f32079e97bc3f92c35db781265a3d131
saran7237 <36067371+saran7237@users.noreply.github.com>
2018-02-21 16:49:00 +0530

    Create 3p.21.2.c

---
+++ 3p.21.2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n;
+printf("enter the even number");
+scanf("%d",&a);
+for(i=1;i<=10;i++);
+printf("enter the odd number");
+scanf("%d",&a);
+getch();
+return 0;
+}
